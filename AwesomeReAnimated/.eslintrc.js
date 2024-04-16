93e9b317dfe0c2fcb34a517177b0fb175c92788a
Yumin Chu <ym.chu@lifesemantics.kr>
2024-04-16 16:24:20 +0900

    Feat: React Native Reanimated을 사용해서 간단한 animation 구현해보기

--- AwesomeReAnimated/.eslintrc.js
+++ AwesomeReAnimated/.eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    'prettier/prettier': 0,
+    'react-native/no-inline-styles': 0,
+    'react-hooks/exhaustive-deps': 'warn',
+  },
 };
