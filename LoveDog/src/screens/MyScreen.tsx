60a0c0281b3e5f534b73f472e3dbb9388b99e429
Yumin Chu <yuminc03@gmail.com>
2024-04-17 23:07:30 +0900

    Feat: MyScreen - 히스토리 화면으로 이동 Button 추가

--- LoveDog/src/screens/MyScreen.tsx
+++ LoveDog/src/screens/MyScreen.tsx
@@ -1,13 +1,27 @@
 import React from "react";
 import { View } from "react-native";
+
 import { Header } from "../components/Header/Header";
+import { Button } from "../components/Button";
+import { Typography } from "../components/Typography";
+import { useRootNavigation } from "../navigation/RootStackNavigation";
 
 export const MyScreen: React.FC = () => {
+  const rootNavigation = useRootNavigation<'Main'>();
+
   return (
     <View style={{ flex: 1 }}>
       <Header>
         <Header.Title title="MyScreen"/>
       </Header>
+
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Button onPress={() => {
+            rootNavigation.push('HistoryList');
+        }}>
+          <Typography fontSize={16}>히스토리 화면으로 이동</Typography>
+        </Button>
+      </View>
     </View>
   );
 };
