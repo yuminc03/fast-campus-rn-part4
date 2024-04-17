bfb58b080a49520ad648c3ba734b9aad76faab48
Yumin Chu <yuminc03@gmail.com>
2024-04-17 11:24:24 +0900

    Refactor: 기존 reanimated 프로젝트 삭제 후 최신 버전으로 다시 프로젝트 만들기

--- AwesomeReAnimated/.eslintrc.js
+++ AwesomeReAnimated/.eslintrc.js
@@ -1,9 +1,4 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
-  rules: {
-    'prettier/prettier': 0,
-    'react-native/no-inline-styles': 0,
-    'react-hooks/exhaustive-deps': 'warn',
-  },
+  extends: '@react-native',
 };
