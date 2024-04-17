fa27c0dc95dc219ae0d50e298f3f52598f2996b1
Yumin Chu <yuminc03@gmail.com>
2024-04-17 16:51:31 +0900

    Feat: InputEmailScreen에서 InputName 화면으로 넘어갈 때 사용할 Button 배치

--- LoveDog/src/screens/InputEmailScreen.tsx
+++ LoveDog/src/screens/InputEmailScreen.tsx
@@ -1,13 +1,30 @@
 import React from "react";
 import { View } from "react-native";
 import { Header } from "../components/Header/Header";
+import { useSignupNavigation, useSignupRoute } from "../navigation/SignupNavigation";
+import { Typography } from "../components/Typography";
+import { Button } from "../components/Button";
 
 export const InputEmailScreen: React.FC = () => {
+  const navigation = useSignupNavigation<'InputEmail'>();
+  const routes = useSignupRoute<'InputEmail'>();
+
   return (
     <View style={{ flex: 1 }}>
       <Header>
         <Header.Title title="InputEmailScreen"/>
       </Header>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Button onPress={() => {
+            navigation.push('InputName', {
+              uid: '',
+              preInput: routes.params.preInput,
+              inputEmail: '',
+            });
+          }}>
+            <Typography fontSize={16}>회원가입 화면으로 이동하기</Typography>
+          </Button>
+      </View>
     </View>
   );
 };
