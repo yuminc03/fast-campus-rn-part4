ca1b369f76b98f20055aaaed1ab71b26c7d7ee7b
Yumin Chu <yuminc03@gmail.com>
2024-04-17 17:03:23 +0900

    Feat: InputNameScreen - 회원가입 완료 버튼 추가, 누르면 Main화면으로 바꾸기

--- LoveDog/src/screens/InputNameScreen.tsx
+++ LoveDog/src/screens/InputNameScreen.tsx
@@ -1,13 +1,25 @@
 import React from "react";
 import { View } from "react-native";
 import { Header } from "../components/Header/Header";
+import { Button } from "../components/Button";
+import { Typography } from "../components/Typography";
+import { useRootNavigation } from "../navigation/RootStackNavigation";
 
 export const InputNameScreen: React.FC = () => {
+  const rootNavigation = useRootNavigation<'Signup'>();
+
   return (
     <View style={{ flex: 1 }}>
       <Header>
         <Header.Title title="InputNameScreen"/>
       </Header>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Button onPress={() => {
+            rootNavigation.replace('Main');
+        }}>
+          <Typography fontSize={16}>회원가입 완료</Typography>
+        </Button>
+      </View>
     </View>
   );
 };
