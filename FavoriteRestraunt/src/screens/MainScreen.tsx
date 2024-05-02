2314b1d26f149a5de870f25445921524a2c4b59b
Yumin Chu <yuminc03@gmail.com>
2024-05-02 11:21:06 +0900

    Feat: 현재 위치에 Marker를 찍기, ios pod 오류 수정

--- FavoriteRestraunt/src/screens/MainScreen.tsx
+++ FavoriteRestraunt/src/screens/MainScreen.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {View} from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, {Marker} from 'react-native-maps';
 
 import {Header} from '../components/Header/Header';
+import Geolocation from '@react-native-community/geolocation';
 
 export const MainScreen: React.FC = () => {
+  const [currentRegion, setCrurentRegion] = useState<{
+    latitude: number;
+    longitude: number;
+  }>({
+    latitude: 37.560214,
+    longitude: 126.9775521,
+  });
+
+  const getMyLocation = useCallback(() => {
+    Geolocation.getCurrentPosition(position => {
+      setCrurentRegion({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      });
+    });
+  }, []);
+
+  useEffect(() => {
+    getMyLocation();
+  }, [getMyLocation]);
+
   return (
     <View style={{flex: 1}}>
       <Header>
@@ -13,10 +35,16 @@ export const MainScreen: React.FC = () => {
       <MapView
         style={{flex: 1}}
         region={{
-          latitude: 37.560214,
-          longitude: 126.9775521,
+          latitude: currentRegion.latitude,
+          longitude: currentRegion.longitude,
           latitudeDelta: 0.015,
           longitudeDelta: 0.0121,
+        }}
+      />
+      <Marker
+        coordinate={{
+          latitude: currentRegion.latitude,
+          longitude: currentRegion.longitude,
         }}
       />
     </View>
