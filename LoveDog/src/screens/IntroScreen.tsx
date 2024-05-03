73bdcdf3cf1b34435904a6cdddfaf38f31e93fa2
Yumin Chu <yuminc03@gmail.com>
2024-05-03 14:31:19 +0900

    Feat: Intro화면에서 Google 로그인 후 로그인 성공 시 InputEmail화면으로 넘어감

--- LoveDog/src/screens/IntroScreen.tsx
+++ LoveDog/src/screens/IntroScreen.tsx
@@ -1,43 +1,57 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import {View} from 'react-native';
+import auth from '@react-native-firebase/auth';
 
 import {Header} from '../components/Header/Header';
 import {Button} from '../components/Button';
 import {Typography} from '../components/Typography';
 import {useRootNavigation} from '../navigation/RootStackNavigation';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+} from '@react-native-google-signin/google-signin';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 export const IntroSceen: React.FC = () => {
   const rootNavigation = useRootNavigation<'Intro'>();
+  const safeArea = useSafeAreaInsets();
+
+  const onPressGoogleSignin = useCallback(async () => {
+    const isSignIn = await GoogleSignin.isSignedIn();
+    if (isSignIn) {
+      await GoogleSignin.signOut();
+    }
+
+    const result = await GoogleSignin.signIn({});
+    const googleCredential = auth.GoogleAuthProvider.credential(result.idToken);
+    const authResult = await auth().signInWithCredential(googleCredential);
+
+    rootNavigation.push('Signup', {
+      screen: 'InputEmail',
+      params: {
+        preInput: {
+          email: result.user.email,
+          name: result.user.name ?? 'Unknown',
+          profileImage: result.user.photo ?? '',
+        },
+        uid: authResult.user.uid,
+      },
+    });
+  }, [rootNavigation]);
 
   return (
     <View style={{flex: 1}}>
       <Header>
         <Header.Title title="IntroSceen" />
       </Header>
-
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Button
-          onPress={() => {
-            rootNavigation.push('Signup', {
-              screen: 'InputEmail',
-              params: {
-                uid: '',
-                preInput: {
-                  email: 'test@test.com',
-                  name: 'test',
-                  profileName: '',
-                },
-              },
-            });
-          }}>
-          <Typography fontSize={16}>회원가입 화면으로 이동하기</Typography>
-        </Button>
-        <Button
-          onPress={() => {
-            rootNavigation.replace('Main');
-          }}>
-          <Typography fontSize={16}>메인 화면으로 이동하기</Typography>
-        </Button>
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+          paddingBottom: 32 + safeArea.bottom,
+        }}>
+        <GoogleSigninButton onPress={onPressGoogleSignin} />
       </View>
     </View>
   );
