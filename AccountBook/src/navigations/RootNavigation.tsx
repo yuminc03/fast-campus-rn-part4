7cd0fad71f47b2196c8047d0c398af7610849e30
Yumin Chu <yuminc03@gmail.com>
2024-05-21 10:39:02 +0900

    Feat: 사용할 가계부 데이터 model 생성, 예시 dummy data 만들기

--- AccountBook/src/navigations/RootNavigation.tsx
+++ AccountBook/src/navigations/RootNavigation.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {
+  NativeStackNavigationProp,
+  createNativeStackNavigator,
+} from '@react-navigation/native-stack';
 
 import {MainScreen} from '../screens/MainScreen';
 import {AddUpdateScreen} from '../screens/AddUpdateScreen';
 import {DetailScreen} from '../screens/DetailScreen';
 import {MonthlyScreen} from '../screens/MonthlyScreen';
+import {AccountBookHistory} from '../data/AccountBookHistory';
+import {RouteProp, useNavigation, useRoute} from '@react-navigation/native';
+
+type ScreenParams = {
+  Add: undefined;
+  Main: undefined;
+  Update: {item: AccountBookHistory};
+  Deteail: {item: AccountBookHistory};
+  MonthlyAverage: undefined;
+};
 
 const Stack = createNativeStackNavigator();
 
@@ -20,3 +33,10 @@ export const RootNavigation = () => {
     </Stack.Navigator>
   );
 };
+
+// RouteName은 ScreenParams의 key들만 받을 수 있음
+export const useRootNavigation = <RouteName extends keyof ScreenParams>() =>
+  useNavigation<NativeStackNavigationProp<ScreenParams, RouteName>>();
+
+export const useRootRoute = <RouteName extends keyof ScreenParams>() =>
+  useRoute<RouteProp<ScreenParams, RouteName>>();
--- AccountBook/src/screens/MainScreen.tsx
+++ AccountBook/src/screens/MainScreen.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View} from 'react-native';
 
 import {Header} from '../components/Header/Header';
+import {AccountBookHistory} from '../data/AccountBookHistory';
+
+const now = new Date().getTime();
 
 export const MainScreen: React.FC = () => {
+  const [list] = useState<AccountBookHistory[]>([
+    {
+      id: 0,
+      type: '사용',
+      price: 10000,
+      comment: 'TEST_01',
+      createdAt: now,
+      updatedAt: now,
+      photoUrl: null,
+    },
+    {
+      id: 1,
+      type: '수입',
+      price: 10000,
+      comment: 'TEST_02',
+      createdAt: now,
+      updatedAt: now,
+      photoUrl:
+        'https://docs.expo.dev/static/images/tutorial/background-image.png',
+    },
+  ]);
+
   return (
     <View style={{flex: 1}}>
       <Header>
