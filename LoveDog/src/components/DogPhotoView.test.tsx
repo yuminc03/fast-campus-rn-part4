2047fcdec62e09d4e74f146a95fdf705396a79b6
Yumin Chu <yuminc03@gmail.com>
2024-05-23 14:22:40 +0900

    Feat: DogPhotoView 테스트 후 snapshot Image source uri가 TEST_MESSAGE인지 확인

--- LoveDog/src/components/DogPhotoView.test.tsx
+++ LoveDog/src/components/DogPhotoView.test.tsx
@@ -1,12 +1,32 @@
 import {render, screen, waitFor} from '@testing-library/react-native';
+import MockAdapter from 'axios-mock-adapter';
 
 import {DogPhotoView} from './DogPhotoView';
+import {api} from '../utils/AxiosUtils';
 
-test('DogPhotoView Render Test', async () => {
-  const component = render(<DogPhotoView />);
+describe('DogPhoto Render Test', () => {
+  let mock: MockAdapter;
 
-  // 특정 component를 찾을 때까지 기다림 (2초 동안)
-  await waitFor(() => component.findByTestId('image'), {timeout: 2000});
+  // 모든 테스트가 돌기 전 실행됨
+  beforeAll(() => {
+    mock = new MockAdapter(api);
+  });
 
-  expect(screen.toJSON()).toMatchSnapshot();
+  // 모든 테스트가 끝났을 때
+  afterAll(() => {
+    mock.reset();
+  });
+
+  test('DogPhotoView Render Snapshot', async () => {
+    mock.onGet(new RegExp('/breeds/image/random')).reply(200, {
+      message: 'TEST_MESSAGE',
+      status: 'SUCCESS',
+    });
+
+    const component = render(<DogPhotoView />);
+    // 특정 component를 찾을 때까지 기다림 (2초 동안)
+    await waitFor(() => component.findByTestId('image'), {timeout: 2000});
+
+    expect(screen.toJSON()).toMatchSnapshot();
+  });
 });
