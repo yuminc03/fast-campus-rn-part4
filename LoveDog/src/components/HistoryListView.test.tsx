966510132a4429c2e641e626364c422314a11f21
Yumin Chu <yuminc03@gmail.com>
2024-05-23 15:38:34 +0900

    Feat: redux--mock-store 설치, HistoryListView 만들고 테스트

---
+++ LoveDog/src/components/HistoryListView.test.tsx
@@ -0,0 +1,58 @@
+import {render, waitFor} from '@testing-library/react-native';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import {Provider} from 'react-redux';
+
+import {HistoryListView} from './HistoryListView';
+
+// database에서 ref().once() 해서 currentHistory 값을 받아오는 것을 mocking
+jest.mock('@react-native-firebase/database', () => {
+  return () => ({
+    ref: jest.fn().mockImplementation(() => ({
+      once: jest.fn().mockReturnValue(
+        Promise.resolve({
+          val: jest.fn().mockReturnValue({
+            TEST: {
+              url: 'test',
+            },
+          }),
+        }),
+      ),
+    })),
+  });
+});
+
+describe('HistoryListView Render Test', () => {
+  const initialState = {};
+  const mockStore = configureStore([thunk]);
+
+  //@ts-ignore
+  let store;
+
+  beforeAll(() => {
+    store = mockStore({
+      user: {
+        user: {
+          uid: 'TEST_ID',
+        },
+      },
+    });
+  });
+
+  test('HistoryLstView Render Sanpshot Test', async () => {
+    const onPressItem = jest.fn();
+
+    const component = render(
+      //ts-ignore
+      <Provider store={store}>
+        <HistoryListView onPressItem={onPressItem} />
+      </Provider>,
+    );
+
+    await waitFor(() => component.findByTestId('Button0'), {
+      timeout: 2000,
+    });
+
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+});
